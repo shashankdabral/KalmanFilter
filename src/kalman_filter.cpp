850f34e058e1a8d2e095efe152adfb77ec518a67
Shashank Dabral <a0393223@ti.com>
2017-11-29 11:21:10 -0600

    bug fix 3

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -102,7 +102,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 
   // New estimate
   x_  = x_ + (K*y);
-  long x_size = x.size();
+  long x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size,x_size);
   P_ = (I - K*Hj_)* P_;
 
